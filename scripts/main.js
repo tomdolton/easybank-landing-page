6af8820b51944f7a9b9db0aab6f50ea5fe3bcdab
Tom Dolton <tom@toms-mbp.home>
2020-04-27 14:29:54 +0100

    Fix mobile dropdown menu on window resize

--- scripts/main.js
+++ scripts/main.js
@@ -6,17 +6,31 @@ const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".nav__menu");
 const modalBackground = document.querySelector(".modal-background");
 
+
+
 // Toggles "active" classes on nav elements to view or hide them
-// Called when hamburger button is clicked
 function toggleNav() {
   hamburger.classList.toggle("hamburger--active");
   menu.classList.toggle("nav__menu--active");
   modalBackground.classList.toggle("modal-background--active");
 }
 
-// If model background is clicke when open, it closes
-window.onclick = function (e) {
-  if (e.target == modalBackground) {
-    toggleNav();
-  }
+// Removes "active" classes on nav elements to hide them
+function closeNav() {
+  hamburger.classList.remove("hamburger--active");
+  menu.classList.remove("nav__menu--active");
+  modalBackground.classList.remove("modal-background--active");
 }
+
+
+
+// Toggle menu when hamburger is clicked
+hamburger.addEventListener("click", toggleNav);
+
+// If modal background is clicked when open, menu closes
+window.onclick = (e) => {
+  if (e.target == modalBackground) closeNav();
+}
+
+// If window is resized, menu closes
+window.onresize = () => closeNav();
